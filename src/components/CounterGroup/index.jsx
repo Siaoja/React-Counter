5c152400eb0f41cbda93722c696e79c4893b3735
SPIKE LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUSP@OOCL.COM>
2020-08-03 16:05:53 +0800

    [counter][spike lu] feature: add counterAmount attribute to dynamic generate counter

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -6,14 +6,27 @@ class CounterGroup extends React.Component {
     constructor(props) {
         console.log("构造函数")
         super(props);
+        this.state = {
+            counterAmount: 0
+        }
+    }
+
+    showCounter = (e) => {
+        this.setState({
+            counterAmount: parseInt(e.target.value)
+        })
+        
+        console.log("change counterAmount:" + this.state.counterAmount);
     }
 
 
     render() {
         return (
             <div>
+
+                <span>counter amount:      </span><input type="text" onChange={this.showCounter.bind(this)}></input>
                 {
-                    new Array(5).fill(0).map((value,index) => {
+                    new Array(this.state.counterAmount).fill(0).map((value,index) => {
                         return (
                             <Counter key={index} />
                         )
